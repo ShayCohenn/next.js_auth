c44309646157a8b0e6348fb7a871bffea284008b
ShayCohenn <shay91847@gmail.com>
2024-01-19 15:55:54 +0200

    middleware and login started & nextAuth installed

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,7 @@ import * as z from "zod";
 import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
 import { ObjectId } from "mongodb";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -18,11 +19,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const generatedId = new ObjectId();
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) return { error: "Email already exists!" };
 
